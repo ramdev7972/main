4067a4d5570259e859e84e5c18c71f2596a37051
ramdev7972 <99469398+ramdev7972@users.noreply.github.com>
2022-02-12 19:08:32 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "us-east-1"
 access_key = "AKIAWGAGKTXMBCNW6N4E"
-secrete_key = "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"
+secret_key = "1J7YOOCdmznw767Xun+WphsGtY5c0jIJasPHP5dD"
 }
 resource "aws_s3_bucket" "empotd" {
 bucket = "krudra"
