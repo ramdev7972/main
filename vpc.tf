3289ea5b8222aa8a05bc0e9f6fcbc6a558dce4fb
ramdev7972 <99469398+ramdev7972@users.noreply.github.com>
2022-02-12 22:55:07 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAWGAGKTXMBSZFGPPS"
-  secret_key = "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"
+  region     = "uast-1"
+  access_key = "AKIAWGAGKTXMBSZPS"
+  secret_key = "X6qudZVl/v0dqeykJDu8F/huuUCp/GSQdTYH"
 }
 
 resource "aws_vpc" "mainsss" {
@@ -18,5 +18,14 @@ resource "aws_s3_bucket" "xyvsd" {
 
   tags = {
     Name        = "My bucket"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
   }
 }
