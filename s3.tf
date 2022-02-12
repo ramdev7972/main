5e70b0d4489274986c9860adac0086090dc9779d
ramdev7972 <99469398+ramdev7972@users.noreply.github.com>
2022-02-12 19:13:56 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 access_key = "AKIAWGAGKTXMBCNW6N4E"
 secret_key = "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"
 }
-resource "aws_s3_bucket" "empotd" {
-bucket = "krudra"
+resource "aws_s3_bucket" "yvesa12" {
+bucket = "krudra22"
 acl = "private"
 }
