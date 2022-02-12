6224ede6474b2aeb5ee4d5253a0229e6fd6120ce
ramdev7972 <99469398+ramdev7972@users.noreply.github.com>
2022-02-12 22:09:39 +0530

    Delete s3.tf

--- s3.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-region = "us-east-1"
-access_key = "AKIAWGAGKTXMBCNW6N4E"
-secret_key = "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"
-}
-resource "aws_s3_bucket" "yvesa12" {
-bucket = "krudra22"
-}
